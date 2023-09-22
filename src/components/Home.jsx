434f3253a37a95f087e1e68254b91030c79c6882
Pravin <pravin08pawar@gmail.com>
2023-09-22 19:15:53 +0530

    changes done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,80 +1,88 @@
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
-import { Box, Heading, Img, SimpleGrid, Spinner, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Img,
+  SimpleGrid,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
 const style = {
-    border: "1px solid green",
-    margin: 6,
-    padding: 8,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
 };
 
 function Home() {
-    const [items, setItems] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
+  const fetchMoreData = () => {
+    if (loading) return; // Don't fetch data if already loading
+    setLoading(true);
 
-        // Function to fetch data from the API
-        const fetchData = async () => {
-            try {
+    // Introduce a 1-second delay before fetching more data
+    setTimeout(async () => {
+      try {
+        const response = await axios.get(
+          `https://63f454bc2213ed989c409c2f.mockapi.io/items?page=${currentPage}&limit=10`
+        );
 
-                // Simulate fetching data after 1 second
-                await new Promise((resolve) => setTimeout(resolve, 2000));
+        const newItems = response.data;
 
-                const response = await axios.get(
-                    `https://63f454bc2213ed989c409c2f.mockapi.io/items?page=${currentPage}&limit=10`
-                );
+        if (newItems.length === 0) {
+          setHasMore(false);
+        } else {
+          setItems([...items, ...newItems]);
+          setCurrentPage(currentPage + 1);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }, 1000); // 1-second delay
+  };
 
-                const newItems = response.data;
+  useEffect(() => {
+    // Initial data fetch
+    fetchMoreData();
+  }, []);
 
-                if (newItems.length === 0) {
-
-                    // If there are no more items to load, set hasMore to false
-                    setHasMore(false);
-                } else {
-
-                    // Append new items to the existing items
-                    setItems([...items, ...newItems]);
-                    setCurrentPage(currentPage + 1);
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        // Fetch initial data when the component mounts
-        fetchData();
-    }, [currentPage, items]);
-
-    const fetchMoreData = () => {
-        // The useEffect above will take care of fetching more data when this function is called.
-    };
-    return (
-        <Box>
-            <Heading>React-infinite-scroll-component</Heading>
-            <hr />
-            <InfiniteScroll
-                dataLength={items.length}
-                next={fetchMoreData}
-                hasMore={hasMore}
-                loader={<Spinner speed="1s" color='blue.500' emptyColor='gray.200' />}
-                scrollThreshold={0.9}
-
-            >
-                <SimpleGrid columns={2} width="70%" margin="auto">
-                    {items.map((item, index) => (
-                        <VStack style={style} key={item.id}>
-                            <Img src={item.image}></Img>
-                            <Text>{item.name}</Text>
-                            <Text>{item.description}</Text>
-                        </VStack>
-                    ))}
-                </SimpleGrid>
-
-            </InfiniteScroll>
-        </Box>
-    );
+  return (
+    <Box>
+      <Heading>React-infinite-scroll-component</Heading>
+      <hr />
+      <InfiniteScroll
+        dataLength={items.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<Spinner speed="1s" color="blue.500" emptyColor="gray.200" />}
+        scrollThreshold={0.9}
+      >
+        <SimpleGrid columns={2} width="70%" margin="auto">
+          {items.map((item, index) => (
+            <VStack style={style} key={item.id}>
+              <Img src={item.image}></Img>
+              <Text>{item.name}</Text>
+              <Text>{item.description}</Text>
+            </VStack>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
+    </Box>
+  );
 }
 
 export default Home;
+
+
+
+
+
